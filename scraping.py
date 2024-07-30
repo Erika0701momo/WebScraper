da5b50c7f5d44f44224f53d3e8c818ac24172b6f
Erika Inoue <kinako771@gmail.com>
2024-07-30 13:45:06 +0900

    エラー対処、プロキシ設定追加

--- scraping.py
+++ scraping.py
@@ -4,29 +4,36 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.robotparser
 from seleniumwire import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+import datetime
 
 class Scraping:
-    URI = {"公開中": "https://filmarks.com/list/now", "公開予定":"https://filmarks.com/list/coming"}
-    movie_info = []
-    today = datetime.datetime.now().strftime("%Y.%m.%d")
-
+    def __init__(self):
+        self.URI = {"公開中": "https://filmarks.com/list/now", "公開予定":"https://filmarks.com/list/coming"}
+        self.movie_info = []
+        self.today = datetime.datetime.now().strftime("%Y.%m.%d")
+        self.done = False
+        self.PROXY = "<http://localhost:8080>"
     # ヘッダーをconfigureするためのリクエストインターセプタを定義
     def interceptor(self, request):
-        request.headers["Accept-Language"] = "en-US,en;q=0.9,ja;q=0.8"
-        request.headers["Referer"] = "https://www.google.com/"
+        # request.headers["Accept-Language"] = "en-US,en;q=0.9,ja;q=0.8"
+        # request.headers["Referer"] = "https://www.google.com/"
 
         del request.headers["User-Agent"]
-        del request.headers["Sec-Ch-Ua"]
-        del request.headers["Sec-Fetch-Site"]
-        del request.headers["Accept-Encoding"]
+        # del request.headers["Sec-Ch-Ua"]
+        # del request.headers["Sec-Fetch-Site"]
+        # del request.headers["Accept-Encoding"]
 
         request.headers[
             "User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-        request.headers["Sec-Ch-Ua"] = "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\""
-        request.headers["Sec-Fetch-Site"] = "cross-site"
-        request.headers["Accept-Encoding"] = "gzip, deflate, br, zstd"
+        # request.headers["Sec-Ch-Ua"] = "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\""
+        # request.headers["Sec-Fetch-Site"] = "cross-site"
+        # request.headers["Accept-Encoding"] = "gzip, deflate, br, zstd"
 
     def get_pace_choice(self):
         page_choice = input(
@@ -38,21 +45,43 @@ class Scraping:
         return page_choice
 
     def initialize_driver(self):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--window-size=1920,1080")
-        service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        # プロキシ設定
+        webdriver.DesiredCapabilities.EDGE["proxy"] = {
+            'proxyType': 'MANUAL',
+            'httpProxy': self.PROXY,
+            'sslProxy': self.PROXY,
+            'http2': False
+        }
+
+        edge_options = webdriver.EdgeOptions()
+        # edge_options.add_argument("--headless")
+        edge_options.add_argument("--no-sandbox")
+        edge_options.add_argument("--disable-dev-shm-usage")
+        edge_options.add_argument("--disable-gpu")
+        edge_options.add_argument("--window-size=1920,1080")
+        self.driver = webdriver.Edge(options=edge_options)
         self.driver.request_interceptor = self.interceptor
+
 
     # ページからhtmlを抜き出す
     def get_page(self,url):
+        self.driver.implicitly_wait(30)
+        print(f"get開始前{datetime.datetime.now()}")
         self.driver.get(url)
-        soup = BeautifulSoup(self.driver.page_source, "html.parser")
-        return soup
+        try:
+            print(f"element取得前{datetime.datetime.now()}")
+            elements = self.driver.find_elements(By.CSS_SELECTOR, ".p-content-cassette__info")
+            print(f"element取得後{datetime.datetime.now()}")
+            if len(elements) == 0:
+                self.driver.quit()
+                self.done = True
+                print("elementが0だったのでquitしました")
+        except NoSuchElementException:
+            print("エレメントがありません")
+            self.driver.quit()
+            print("quitしました")
+            self.done = True
+        print(f"get後{datetime.datetime.now()}")
 
     # 映画の情報を辞書として格納
     def get_info(self, element, link, length):
@@ -86,36 +115,41 @@ class Scraping:
         if is_scraping_ok:
             page = 1
             while True:
-                url = self.URI[page_choice] + "?page=" + str(page)
-                soup = self.get_page(url)
-                print(page)
-                if not soup.select(".p-content-cassette__info"):
-                    break
-                for element in soup.select(".p-content-cassette__info"):
-                    # 作品詳細のリンクがない映画の対策
-                    link_element = element.select_one(".p-content-cassette__people__readmore a")
-                    if link_element:
-                        link = "https://filmarks.com" + link_element.get("href")
-                    else:
-                        link = None
-                    # 上映時間がない映画の対策
-                    length_element = element.select_one(".up-screen_and_country span:nth-of-type(2)")
-                    if length_element:
-                        length = length_element.getText()
-                    else:
-                        length = None
-
-                    self.movie_info.append(self.get_info(element, link, length))
+                try:
+                    url = self.URI[page_choice] + "?page=" + str(page)
+                    self.get_page(url)
+                    if self.done:
+                        break
+                    soup = BeautifulSoup(self.driver.page_source, "html.parser")
+                    print(page)
+                    for element in soup.select(".p-content-cassette__info"):
+                        # 作品詳細のリンクがない映画の対策
+                        link_element = element.select_one(".p-content-cassette__people__readmore a")
+                        if link_element:
+                            link = "https://filmarks.com" + link_element.get("href")
+                        else:
+                            link = None
+                        # 上映時間がない映画の対策
+                        length_element = element.select_one(".up-screen_and_country span:nth-of-type(2)")
+                        if length_element:
+                            length = length_element.getText()
+                        else:
+                            length = None
+                        movie = self.get_info(element, link, length)
+                        print(movie["title"])
+                        self.movie_info.append(self.get_info(element, link, length))
+                except Exception as e:
+                    print(e)
+                    self.driver.quit()
 
                 page += 1
-            self.driver.quit()
+
 
     def make_csv(self, page_choice):
          # csvを出力
         df = pd.DataFrame(self.movie_info)
         df.index = df.index + 1
         df.to_csv(f"{page_choice}Filmarksランキング{self.today}.csv", encoding="utf-8_sig")
-
 
 
 
